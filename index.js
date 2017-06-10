74eefdd528037d5dfdc76f68624c5e55fdab389d
Ko0lGuY <bha0011@jmss.vic.edu.au>
2017-06-10 20:25:41 +1000

    verify change

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.get('/webhook/', function (req, res) {
     if (req.query['hub.verify_token'] === 'thisistoken') {
         res.send(req.query['hub.challenge'])
     }
-    res.send('No sir')
+    res.send('No sir2')
 })
 
 // Spin up the server
