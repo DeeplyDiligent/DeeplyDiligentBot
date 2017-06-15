9c3a32b3879de87b1739fc5246d79f87ac003fa5
Ko0lGuY <bha0011@jmss.vic.edu.au>
2017-06-15 18:26:04 +1000

    too much loggig

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.post('/webhook', function (req, res) {
         if (event.message) {
           receivedMessage(event);
         } else {
-          console.log("Webhook received unknown event: ", event);
+          //console.log("Webhook received unknown event: ", event);
         }
       });
     });
