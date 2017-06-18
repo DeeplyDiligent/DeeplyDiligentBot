a7065be4ecad7036c770a16468382c94b9a702e2
Ko0lGuY <bha0011@jmss.vic.edu.au>
2017-06-18 19:23:37 +1000

    dictionary needs to be parsed :disappointed:

--- index.js
+++ index.js
@@ -215,7 +215,7 @@ function retrieveReminders(){
       console.log("importing these reminders: ");
         console.log(rows[0].dat);
         reminders = rows[0].dat;
-        console.log(reminders["1844872135526609"]);
+        console.log(reminders["1844872135526609"]);//TODO: THIS DOESNT WORK >.< parse the dictionary somehow
     });
     connection.end();
 }
