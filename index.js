58a10e0e6234fa64c19e727e0773b96d8af6e95f
Ko0lGuY <bha0011@jmss.vic.edu.au>
2017-06-18 19:11:05 +1000

    trying to retrieve all reminders and set it to reminder variable

--- index.js
+++ index.js
@@ -213,7 +213,7 @@ function retrieveReminders(){
     connection.query('SELECT * FROM Customers LIMIT 0,1;', function(err, rows, fields) {
       if (err) throw err;
       console.log("this is what i got");
-        console.log(rows);
+        console.log(rows.RowDataPacket);
         console.log(fields);
     });
     connection.end();
