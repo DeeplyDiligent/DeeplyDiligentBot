19e007b5939bca2803d0b7de245d20bb105b19ad
Ko0lGuY <bha0011@jmss.vic.edu.au>
2017-06-19 09:41:39 +1000

    deleting after reminder done

--- index.js
+++ index.js
@@ -234,14 +234,17 @@ function toTimeZone(whatzone) {
 function checkIfAnyOverdueReminders(sendto){
     //TODO: AUS ONLY AT THE MOMENT, EXPAND
     var time = toTimeZone("Australia/Melbourne");
-    if (reminders[sendto] != undefined){
-        reminders[sendto].forEach(isoverdue);
+    var copyofremindersforsendto = reminders[sendto].slice();
+    if (copyofremindersforsendto != undefined){
+        copyofremindersforsendto.forEach(isoverdue);
         function isoverdue(onetime, index){
             console.log('checking if ' + onetime.substring(0,2) +"<="+time[0] + " and "+ onetime.substring(3,5)+"<="+time[1]);
             console.log(parseInt(onetime.substring(0,2)) <= time[0] && parseInt(onetime.substring(3,5)) <= time[1]);
             if (parseInt(onetime.substring(0,2)) <= time[0] && parseInt(onetime.substring(3,5)) <= time[1]){
                 sendTextMessage(sendto,"it is "+ time[0]+":"+time[1]+ " and i am reminding you of " + onetime)
                 console.log('deleting reminder at '+ onetime);
+                var indexofonetime = reminders[sendto].indexOf(onetime);
+                reminders[sendto].splice(indexofonetime, 1);
                 //todo: delete the reminder
             }
         }
